251bc46145234fd341762ebb88e951a59e895482
qubka <nikita.ushakov0000@outlook.com>
2026-04-19 16:58:14 +0100

    fix: remove commented log

--- src/core/user_message_manager.cpp
+++ src/core/user_message_manager.cpp
@@ -51,9 +51,6 @@ ResultType UserMessageManager::ExecuteMessageCallbacks(INetworkMessageInternal* 
 	UserMessage message(msgSerializable, msgData, *clients);
 
 	int16_t messageId = message.GetMessageID();
-	
-	//plg::print(LS_DETAILED, "[CUserMessageManager::ExecuteMessageCallbacks][{}] Pushing user message `{}` pointer: %p\n", mode == HookMode::Pre ? "Pre" : "Post",
-	//	messageId, static_cast<const void*>(msgSerializable));
 
 	ResultType result = ResultType::Continue;
 
